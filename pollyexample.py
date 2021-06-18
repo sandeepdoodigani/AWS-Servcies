66fd1bfe0782becab47bfd6dd21d0e31f35ad0be
Android Application Developer <sandeep.doodigani@gmail.com>
2021-06-18 11:35:52 +0530

    Create pollyexample.py

---
+++ pollyexample.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 18 11:25:42 2021
+
+@author: SmartBridgePC
+"""
+
+import boto3
+from pygame import mixer
+import os
+
+#Giving the credentials to gain complete access from the Rekognition API(service)
+access_key_id = 'AKIA3GFGYRQGVRGPDFG4'
+secret_access_key = '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'
+
+#Region
+region='ap-south-1' 
+
+
+polly_client = boto3.client('polly',
+                            region,
+                            aws_access_key_id = access_key_id,
+                            aws_secret_access_key = secret_access_key)
+
+speech = polly_client.synthesize_speech(VoiceId='Joanna',
+                OutputFormat='mp3', 
+                Text = response["TextDetections"][0]["DetectedText"])
+
+with open('output.mp3', 'wb') as f:
+    f.write(speech['AudioStream'].read())
+    f.close()
+
+mixer.init()#initilizing mixer
+mixer.music.load('output.mp3')#load our output
+mixer.music.play()#play the loaded output
+
+#once speaking is done delete the file
+while mixer.music.get_busy() == True:
+    pass
+
+mixer.quit()
+os.remove('output.mp3')
